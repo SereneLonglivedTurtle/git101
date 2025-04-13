b7ec9abf3ba17eb1250b6ce4189ccb9f474f83a8
luckiestkitty <777luckiestkitty777@gmail.com>
2025-04-13 13:59:51 -0700

    Initial commit

---
+++ orange.py
@@ -0,0 +1,12 @@
+def get_orange_info():
+    """
+    Returns information about an orange
+    """
+    return {
+        "name": "Orange",
+        "color": "orange",
+        "type": "fruit"
+    }
+
+if __name__ == "__main__":
+    print(get_orange_info()) 
