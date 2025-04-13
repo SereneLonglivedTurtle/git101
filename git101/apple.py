45bb225dfd94d396eafa1178572a24ff5d9c8f44
luckiestkitty <777luckiestkitty777@gmail.com>
2025-04-13 13:41:04 -0700

    Initial commit

---
+++ git101/apple.py
@@ -0,0 +1,12 @@
+def get_apple_info():
+    """
+    Returns information about an apple
+    """
+    return {
+        "name": "Apple",
+        "color": "red",
+        "type": "fruit"
+    }
+
+if __name__ == "__main__":
+    print(get_apple_info()) 
